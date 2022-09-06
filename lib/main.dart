2cb5dd9fac608c420615430c9e37dcf38a2518b2
Sabir Bugti <77012663+Sabirbugti9@users.noreply.github.com>
2022-09-06 17:38:40 +0500

    updated codes
    updated codes

--- lib/main.dart
+++ lib/main.dart
@@ -47,85 +47,88 @@ class _MyAppState extends State<MyApp> {
 
   Translation? translation;
   String result = "";
+  String gfg = "";
+
   void data() async {
-    String gfg = sms;
-    setState(() {
-      result = gfg.replaceAll("TS", "ც");
+    // String gfg = sms;
+    gfg =
+        "აბონენთის N: 1794794; სახელი გვარი: ოლა Tურმო; TარიRი: 31-AUG-22; სულ გადასახდელია: -53.41 ლარი; ცინა ჩვენება: 30146; 29-JUL-22; ახალი ჩვენება: 30278; 29-AUG-22; მიმდინარე ხარჯი: 132 ქვთ/სT ; მიმდინარე ხარჯი: 29.11 ლარი ; თარიფი: 0.2205302;";
+    result = gfg.replaceAll("TS", "ც");
+    gfg = result;
+    result = gfg.replaceAll("ts", "ც");
+    gfg = result;
+    result = gfg.replaceAll("w", "ვ");
+    gfg = result;
+    result = gfg.replaceAll("a", "ა");
+    gfg = result;
+    result = gfg.replaceAll("b", "ბ");
+    gfg = result;
+    result = gfg.replaceAll("g", "გ");
+    gfg = result;
+    result = gfg.replaceAll("d", "დ");
+    gfg = result;
+    result = gfg.replaceAll("e", "ე");
+    gfg = result;
+    result = gfg.replaceAll("v", "ვ");
+    gfg = result;
+    result = gfg.replaceAll("z", "ზ");
+    gfg = result;
+    result = gfg.replaceAll("t", "თ");
+    gfg = result;
+    result = gfg.replaceAll("i", "ი");
+    gfg = result;
+    result = gfg.replaceAll("kʼ", "კ");
+    gfg = result;
+    result = gfg.replaceAll("l", "ლ");
+    gfg = result;
+    result = gfg.replaceAll("m", "მ");
+    gfg = result;
+    result = gfg.replaceAll("n", "ნ");
+    gfg = result;
+    result = gfg.replaceAll("o", "ო");
+    gfg = result;
+    result = gfg.replaceAll("pʼ", "პ");
+    gfg = result;
+    result = gfg.replaceAll("zh", "ჟ");
+    gfg = result;
+    result = gfg.replaceAll("r", "რ");
+    gfg = result;
+    result = gfg.replaceAll("s", "ს");
+    gfg = result;
+    result = gfg.replaceAll("tʼ", "ტ");
+    gfg = result;
+    result = gfg.replaceAll("u", "უ");
+    gfg = result;
+    result = gfg.replaceAll("p", "ფ");
+    gfg = result;
+    result = gfg.replaceAll("k", "ქ");
+    gfg = result;
+    result = gfg.replaceAll("gh", "ღ");
+    gfg = result;
+    result = gfg.replaceAll("qʼ", "ყ");
+    gfg = result;
+    result = gfg.replaceAll("sh", "შ");
+    gfg = result;
+    result = gfg.replaceAll("ch", "ჩ");
+    gfg = result;
 
-      result = gfg.replaceAll("ts", "ც");
-
-      result = gfg.replaceAll("w", "ვ");
-
-      result = gfg.replaceAll("a", "ა");
-
-      result = gfg.replaceAll("b", "ბ");
-
-      result = gfg.replaceAll("g", "გ");
-
-      result = gfg.replaceAll("d", "დ");
-
-      result = gfg.replaceAll("e", "ე");
-
-      result = gfg.replaceAll("v", "ვ");
-
-      result = gfg.replaceAll("z", "ზ");
-
-      result = gfg.replaceAll("t", "თ");
-
-      result = gfg.replaceAll("i", "ი");
-
-      result = gfg.replaceAll("kʼ", "კ");
-
-      result = gfg.replaceAll("l", "ლ");
-
-      result = gfg.replaceAll("m", "მ");
-
-      result = gfg.replaceAll("n", "ნ");
-
-      result = gfg.replaceAll("o", "ო");
-
-      result = gfg.replaceAll("pʼ", "პ");
-
-      result = gfg.replaceAll("zh", "ჟ");
-
-      result = gfg.replaceAll("r", "რ");
-
-      result = gfg.replaceAll("s", "ს");
-
-      result = gfg.replaceAll("tʼ", "ტ");
-
-      result = gfg.replaceAll("u", "უ");
-
-      result = gfg.replaceAll("p", "ფ");
-
-      result = gfg.replaceAll("k", "ქ");
-
-      result = gfg.replaceAll("gh", "ღ");
-
-      result = gfg.replaceAll("qʼ", "ყ");
-
-      result = gfg.replaceAll("sh", "შ");
-
-      result = gfg.replaceAll("ch", "ჩ");
-
-      result = gfg.replaceAll("dz", "ძ");
-
-      result = gfg.replaceAll("tsʼ", "წ");
-
-      result = gfg.replaceAll("chʼ", "ჭ");
-
-      result = gfg.replaceAll("kh", "ხ");
-
-      result = gfg.replaceAll("j", "ჯ");
-
-      result = gfg.replaceAll("h", "ჰ");
-    });
+    result = gfg.replaceAll("dz", "ძ");
+    gfg = result;
+    result = gfg.replaceAll("tsʼ", "წ");
+    gfg = result;
+    result = gfg.replaceAll("chʼ", "ჭ");
+    gfg = result;
+    result = gfg.replaceAll("kh", "ხ");
+    gfg = result;
+    result = gfg.replaceAll("j", "ჯ");
+    gfg = result;
+    result = gfg.replaceAll("h", "ჰ");
+    gfg = result;
 
     final translator = GoogleTranslator();
 
     // Passing the translation to a variable
-    Translation trans =
-        await translator.translate(result, from: 'ka', to: 'en');
+    Translation trans = await translator.translate(gfg, from: 'ka', to: 'en');
     setState(() {
       translation = trans;
     });
@@ -141,61 +144,63 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Center(child: Text('SMS Reader')),
           backgroundColor: const Color(0xff343351),
+          leading: IconButton(
+              onPressed: () {
+                data();
+              },
+              icon: const Icon(Icons.search)),
         ),
         body: Center(
-          child: translation == null
-              ? const CircularProgressIndicator()
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox.fromSize(),
-                    Column(
-                      children: [
-                        const Text(
-                          'Incoming message:',
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xff343351)),
-                        ),
-                        const SizedBox(height: 18),
-                        Text(
-                          sms,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox.fromSize(),
+              Column(
+                children: [
+                  const Text(
+                    'Incoming message:',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff343351)),
+                  ),
+                  const SizedBox(height: 18),
+                  Text(
+                    gfg,
+                    style: const TextStyle(fontSize: 18, color: Colors.blue),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  const Text(
+                    "Translated message:",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff343351)),
+                  ),
+                  const SizedBox(height: 18),
+                  translation != null
+                      ? Text(
+                          translation!.text.toString(),
                           style:
                               const TextStyle(fontSize: 18, color: Colors.blue),
-                        ),
-                        const SizedBox(
-                          height: 18,
-                        ),
-                        const Text(
-                          "Translated message:",
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xff343351)),
-                        ),
-                        const SizedBox(height: 18),
-                        translation != null
-                            ? Text(
-                                translation!.text.toString(),
-                                style: const TextStyle(
-                                    fontSize: 18, color: Colors.blue),
-                              )
-                            : SizedBox.fromSize()
-                      ],
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Text(
-                        'Based on access_incoming_sms Copyright (c) 2022 Gülsen Keskin',
-                        style: TextStyle(
-                          fontSize: 10,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ],
+                        )
+                      : SizedBox.fromSize()
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  'Based on access_incoming_sms Copyright (c) 2022 Gülsen Keskin',
+                  style: TextStyle(
+                    fontSize: 10,
+                    color: Colors.black,
+                  ),
                 ),
+              ),
+            ],
+          ),
         ),
       ),
     );
