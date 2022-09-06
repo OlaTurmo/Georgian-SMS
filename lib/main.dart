1339376b0ac17ce6179d79b65cbe243a873bde45
Sabir Bugti <77012663+Sabirbugti9@users.noreply.github.com>
2022-09-06 16:16:34 +0500

    updated Language Issue
    updated Language Issue

--- lib/main.dart
+++ lib/main.dart
@@ -47,95 +47,13 @@ class _MyAppState extends State<MyApp> {
 
   Translation? translation;
   void data() async {
-    String gfg = "rs.ge (s/n an p/n) 305516536: 8516 kods vada gasdis 5 wuTSi";
-    String result = gfg;
-    gfg = sms;
-
-    print(gfg.toLowerCase());
-    gfg = result;
-
-    result = gfg.replaceAll("TS", "ც");
-    gfg = result;
-
-    result = gfg.replaceAll("ts", "ც");
-    gfg = result;
-
-    result = gfg.replaceAll("w", "ვ");
-    gfg = result;
-
-    result = gfg.replaceAll("a", "ა");
-    gfg = result;
-    result = gfg.replaceAll("b", "ბ");
-    gfg = result;
-    result = gfg.replaceAll("g", "გ");
-    gfg = result;
-    result = gfg.replaceAll("d", "დ");
-    gfg = result;
-    result = gfg.replaceAll("e", "ე");
-    gfg = result;
-    result = gfg.replaceAll("v", "ვ");
-    gfg = result;
-    result = gfg.replaceAll("z", "ზ");
-    gfg = result;
-    result = gfg.replaceAll("t", "თ");
-    gfg = result;
-    result = gfg.replaceAll("i", "ი");
-    gfg = result;
-    result = gfg.replaceAll("kʼ", "კ");
-    gfg = result;
-    result = gfg.replaceAll("l", "ლ");
-    gfg = result;
-    result = gfg.replaceAll("m", "მ");
-    gfg = result;
-    result = gfg.replaceAll("n", "ნ");
-    gfg = result;
-    result = gfg.replaceAll("o", "ო");
-    gfg = result;
-    result = gfg.replaceAll("pʼ", "პ");
-    gfg = result;
-    result = gfg.replaceAll("zh", "ჟ");
-    gfg = result;
-    result = gfg.replaceAll("r", "რ");
-    gfg = result;
-    result = gfg.replaceAll("s", "ს");
-    gfg = result;
-    result = gfg.replaceAll("tʼ", "ტ");
-    gfg = result;
-    result = gfg.replaceAll("u", "უ");
-    gfg = result;
-    result = gfg.replaceAll("p", "ფ");
-    gfg = result;
-    result = gfg.replaceAll("k", "ქ");
-    gfg = result;
-    result = gfg.replaceAll("gh", "ღ");
-    gfg = result;
-    result = gfg.replaceAll("qʼ", "ყ");
-    gfg = result;
-    result = gfg.replaceAll("sh", "შ");
-    gfg = result;
-    result = gfg.replaceAll("ch", "ჩ");
-    gfg = result;
-
-    result = gfg.replaceAll("dz", "ძ");
-    gfg = result;
-    result = gfg.replaceAll("tsʼ", "წ");
-    gfg = result;
-    result = gfg.replaceAll("chʼ", "ჭ");
-    gfg = result;
-    result = gfg.replaceAll("kh", "ხ");
-    gfg = result;
-    result = gfg.replaceAll("j", "ჯ");
-    gfg = result;
-    result = gfg.replaceAll("h", "ჰ");
-    gfg = result;
-
-    print(gfg);
+    
+    String gfg = sms;
 
     final translator = GoogleTranslator();
-    final input = gfg;
 
     // Passing the translation to a variable
-    Translation trans = await translator.translate(input, from: 'ka', to: 'en');
+    Translation trans = await translator.translate(gfg, from: 'ka', to: 'en');
     setState(() {
       translation = trans;
     });
@@ -150,12 +68,10 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Center(child: Text('SMS Reader')),
-          backgroundColor:  const Color(0xff343351),
+          backgroundColor: const Color(0xff343351),
         ),
         body: Center(
-          child: translation == null
-              ? const CircularProgressIndicator()
-              : Column(
+          child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     SizedBox.fromSize(),
@@ -166,13 +82,13 @@ class _MyAppState extends State<MyApp> {
                           style: TextStyle(
                               fontSize: 24,
                               fontWeight: FontWeight.bold,
-                              color:  Color(0xff343351)),
+                              color: Color(0xff343351)),
                         ),
                         const SizedBox(height: 18),
                         Text(
                           sms,
                           style:
-                              const TextStyle(fontSize: 18, color:Colors.blue),
+                              const TextStyle(fontSize: 18, color: Colors.blue),
                         ),
                         const SizedBox(
                           height: 18,
@@ -182,14 +98,14 @@ class _MyAppState extends State<MyApp> {
                           style: TextStyle(
                               fontSize: 24,
                               fontWeight: FontWeight.bold,
-                              color:  Color(0xff343351)),
+                              color: Color(0xff343351)),
                         ),
                         const SizedBox(height: 18),
                         translation != null
                             ? Text(
                                 translation!.text.toString(),
                                 style: const TextStyle(
-                                    fontSize: 18, color:Colors.blue),
+                                    fontSize: 18, color: Colors.blue),
                               )
                             : SizedBox.fromSize(),
                       ],
